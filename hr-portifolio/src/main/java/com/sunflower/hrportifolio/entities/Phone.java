8f4b3b6bc02ba4c55ef897450586275f4bd374e1
Jackson Almeida <jacksonnalmeida30@gmail.com>
2023-01-20 19:55:50 -0400

    create entity Phone

---
+++ hr-portifolio/src/main/java/com/sunflower/hrportifolio/entities/Phone.java
@@ -0,0 +1,46 @@
+package com.sunflower.hrportifolio.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_phones")
+public class Phone implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 12)
+	private String number;
+
+	public Phone(Long id, String number) {
+		this.id = id;
+		this.number = number;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+}
