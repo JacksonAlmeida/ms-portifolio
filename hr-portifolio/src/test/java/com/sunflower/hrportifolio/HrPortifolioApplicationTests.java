727ac9fa572c6c345aa7dcd0396eb3398e567bab
Jackson Almeida <jacksonnalmeida30@gmail.com>
2023-01-20 17:33:52 -0400

    create hr-portifolio

---
+++ hr-portifolio/src/test/java/com/sunflower/hrportifolio/HrPortifolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunflower.hrportifolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPortifolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
