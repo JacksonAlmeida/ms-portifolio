813555e7b607db499b97f0b9fd4260ae191a9d8b
Jackson Almeida <jacksonnalmeida30@gmail.com>
2023-01-20 19:57:28 -0400

    create PhoneRepository

---
+++ hr-portifolio/src/main/java/com/sunflower/hrportifolio/repositories/PhoneRepository.java
@@ -0,0 +1,9 @@
+package com.sunflower.hrportifolio.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sunflower.hrportifolio.entities.Phone;
+
+public interface PhoneRepository extends JpaRepository<Phone, Long> {
+
+}
