3962d26d5333b5fc55d6441b839419ec0fc72810
Jackson Almeida <jacksonnalmeida30@gmail.com>
2023-01-28 12:55:18 -0400

    hr-portifolio as a config server/client

--- hr-portifolio/src/main/java/com/sunflower/hrportifolio/resources/MessageResource.java
+++ hr-portifolio/src/main/java/com/sunflower/hrportifolio/resources/MessageResource.java
@@ -1,17 +1,21 @@
 package com.sunflower.hrportifolio.resources;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "msg")
+@RequestMapping(value = "api/hr-phone/configs")
 public class MessageResource {
-
+	
+	@Value("${test.config}")
+	private String testConfig;
+	
 	@GetMapping
 	public ResponseEntity<String> message (){
-		String msg = "Hello world!!!";
+		String msg = testConfig;
 		return ResponseEntity.ok().body(msg);
 	}
 	
